914594009402927a7327eaee5e4bcd0abddd5b45
Andrei Tiberiu <andrei.tiberiu@hotmail.com>
2022-09-29 19:46:28 +0300

    update

---
+++ src/main/java/java38team3/ecommerce/services/UserService.java
@@ -0,0 +1,12 @@
+package java38team3.ecommerce.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
