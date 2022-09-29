d7c06642c324d427e0a256ec75f4f24f101f505c
Andrei Tiberiu <andrei.tiberiu@hotmail.com>
2022-09-29 18:23:09 +0300

    Initial commit

---
+++ src/test/java/java38team3/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package java38team3.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
